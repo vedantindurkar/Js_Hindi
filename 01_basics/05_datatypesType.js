31421430992557490d28e059c6676556baeb4340
Vedant Indurkar <indurkarvedant1@gmail.com>
2025-01-31 23:23:07 +0530

    types of datatyaps

---
+++ 01_basics/05_datatypesType.js
@@ -0,0 +1,13 @@
+/*there are two types of datatypes in js 1.primitive
+2.Reference (non primitive) */
+
+//primitive: 7 types =>string,number,boolean,null,undefined,symbol,BigInt
+
+//Reference(non-primitive):
+//arrays,functions,objects
+
+//java script is a dynamically typed language
+
+const myfunction=function(){
+    console.log("Hi I'm Vedant!")
+}
