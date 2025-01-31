cef1f57259be94e09e6634f8665736b493cffee7
Vedant Indurkar <indurkarvedant1@gmail.com>
2025-01-31 14:06:07 +0530

    fisrt change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accoundId=232
+let accountEmail="v@v.com"
+var accountPassword="ram"
+var accountCity;
+
+//accoundId=9  const can't be re-assigned 
+//var should not be used at all
+
+console.table([accoundId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+ "use strict";//treat all JS code as newer version
+ //....alert(4+4) we are using node js not browser so it gives error
+
+ let name="vedant"
+ let age=22
+ let ismale=true
+
+ //null => standalone value means it is a value
+ //undefined=> not defined doesnt have a value
+
+ 
+ 
