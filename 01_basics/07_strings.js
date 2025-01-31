1677674da48a187357523214191b0b8809706954
Vedant Indurkar <indurkarvedant1@gmail.com>
2025-02-01 00:24:51 +0530

    strings

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,26 @@
+const name="vedant Indurkar"
+const thismuch=8
+//above is normal way to declare strings but don't use it, use the new one
+//${name}=>string interpolation
+//console.log(`Hello my name is ${name} and i love to code ${thismuch} hrs a day`)
+
+//console.log(`${name} ${thismuch}`)
+
+let str1="Hi"
+let str2="vedsssssssssant"
+
+//console.log(`${str1} ${str2}`)
+
+//another way to define a string is:
+
+// let string1=new String("Vedant    is a good boy.")
+let string1="          vedant   "
+console.log(string1)
+// console.log(string1.charAt(3))
+// console.log(string1.toUpperCase())
+// console.log(string1)
+// console.log(string1.indexOf('t'))
+console.log(string1.trim())
+
+//these string functions works on string only when its as primitive data type but they dont work when strings are declared in non-primitive fashion.
+
