c476b05587fe7f264c770af1907c8d9dc0e86062
Vedant Indurkar <indurkarvedant1@gmail.com>
2025-01-31 14:32:37 +0530

    one again

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score="77aa"
+let int="8848"
+
+let scon=String(int)//this is how we convert one datatype to another.
+//String(),Number() are used
+
+console.log(typeof int)
+
+let converted=Number(score)
+console.log(typeof score)
+console.log(converted)
