726344013fe9dbd473661f7c7723ca07441081d9
Vedant Indurkar <indurkarvedant1@gmail.com>
2025-01-31 14:50:52 +0530

    data conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,28 @@
-let score="77aa"
+let score="77aa"//this cant be converted to number as it gives a value of type NaN
 let int="8848"
+let hana=true//bool value gets converted to 1 or 0 as a number
+
+let oops=Number(hana)
+
+//console.log(oops)
 
 let scon=String(int)//this is how we convert one datatype to another.
 //String(),Number() are used
 
-console.log(typeof int)
+//console.log(typeof int)
 
+let hey=34
+let typehey=Boolean(hey)
+//console.log(typehey)
+
+//1 => true
+//0 =>false
+//any string ka bool true and empty string ka bool false hota hai
+
+let anum="89t"
+let astring=String(anum)
+console.log(astring)
+//num to string is also possible
 let converted=Number(score)
-console.log(typeof score)
-console.log(converted)
+//console.log(typeof score)
+//console.log(converted)
